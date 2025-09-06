54c50de861d30a454ff267516b468c0466f58bf1
Thiago Mendes do Nascimento <124217018+Offiline26@users.noreply.github.com>
2025-09-05 22:07:50 -0300

    Delete src directory

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.spring_mvc.controller;import br.com.fiap.spring_mvc.entity.Livro;import br.com.fiap.spring_mvc.service.LivroService;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Controller;import org.springframework.ui.Model;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.RequestMapping;import java.util.List;@Controller@RequestMapping("/livros")public class LivroController {    @Autowired    LivroService livroService;    @GetMapping("/lista")    public String listarLivros(Model model) {        List<Livro> livros = livroService.readLivros();        model.addAttribute("listaLivros", livros);        return "livroLista";    }    @GetMapping("/cadastro")    public String cadastroLivro(Model model){        model.addAttribute("livro", new Livro());        return "livroCadastro";    }    @PostMapping("/cadastrar")
-public String cadastrarLivro(Livro livro){
-    livroService.createLivro(livro);
-    return "redirect:/livros/lista"; // garante que a URL mude para /livros/lista
-}}
